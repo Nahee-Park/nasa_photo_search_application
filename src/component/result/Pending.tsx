05cb4449bcbb6f3a9224386917db9a03595338b0
nahee-park <jessica396@ewhain.net>
2021-07-23 07:03:16 +0900

    feat: 로딩중 메시지 퍼블리싱

--- src/component/result/Pending.tsx
+++ src/component/result/Pending.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 function Pending() {
-  return <div>로딩중입니다</div>;
+  return <Styled.Root>로딩중입니다</Styled.Root>;
 }
 
 export default Pending;
+
+const Styled = {
+  Root: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
